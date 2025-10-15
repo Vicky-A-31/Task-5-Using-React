34478998fc7947a2d69d8670d1e26a307d24a282
Vignesh A <118335617+Vicky-A-31@users.noreply.github.com>
2025-10-15 17:59:24 +0530

    Update ColorCode.jsx

--- src/ColorCode.jsx
+++ src/ColorCode.jsx
@@ -1,9 +1,13 @@
 import { Component } from 'react'
 
 export class ColorCode extends Component {
+
   render() {
     return (
-      <p>{`Color Code: ${this.props.color}`}</p>
+        <>
+      <p>{`HEX Color Code: ${this.props.hexColor}`}</p>
+      <p>{`RGB Color Code: rgb(${this.props.rgb[0]}, ${this.props.rgb[1]}, ${this.props.rgb[2]})`}</p>
+      </>
     )
   }
 }
